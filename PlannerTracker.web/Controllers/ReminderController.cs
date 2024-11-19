d686e2b3fdef2090a4b64cfd2fb813eaa6f5772c
femiliapm <femiliaputrimayranti@gmail.com>
2024-11-19 14:46:27 +0700

    layouting reminders module

---
+++ PlannerTracker.web/Controllers/ReminderController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PlannerTracker.ViewModel;
+
+namespace PlannerTracker.web.Controllers
+{
+    public class ReminderController : Controller
+    {
+        public IActionResult Index()
+        {
+            string? authStr = HttpContext.Session.GetString("auth");
+            VMAuth? auth = authStr != null ? JsonConvert.DeserializeObject<VMAuth?>(authStr) : null;
+            if (auth == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewData["Title"] = "Reminders";
+
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            string? authStr = HttpContext.Session.GetString("auth");
+            VMAuth? auth = authStr != null ? JsonConvert.DeserializeObject<VMAuth?>(authStr) : null;
+            if (auth == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Title = "Add Reminder";
+
+            return View();
+        }
+    }
+}
