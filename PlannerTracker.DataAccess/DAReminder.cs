67c50e40092de349af0e3770887c520e6946f5e1
femiliapm <femiliaputrimayranti@gmail.com>
2024-11-20 09:30:00 +0700

    reminder module API : create and fetch all

---
+++ PlannerTracker.DataAccess/DAReminder.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using PlannerTracker.DataModel;
+using PlannerTracker.ViewModel;
+using System.Net;
+
+namespace PlannerTracker.DataAccess
+{
+    public class DAReminder
+    {
+        private readonly PlannerTrackerContext db;
+
+        public DAReminder(PlannerTrackerContext _db)
+        {
+            this.db = _db;
+        }
+
+        public VMResponse<List<VMReminder>> FetchAll()
+        {
+            VMResponse<List<VMReminder>> response = new();
+
+            using (IDbContextTransaction dbTran = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    List<Reminder> reminders = db.Reminders.ToList();
+
+                    reminders.ForEach(r =>
+                    {
+                        if (r.ReminderDate.DayOfYear - DateTime.Now.DayOfYear < 0)
+                        {
+                            r.IsCompleted = true;
+
+                            db.Update(r);
+                            db.SaveChanges();
+                        }
+                    });
+
+                    dbTran.Commit();
+
+                    List<VMReminder> data = (
+                            from r in db.Reminders
+                            where r.IsDelete == false
+                            select new VMReminder(r)
+                        ).ToList();
+
+                    response.Data = data;
+                    response.StatusCode = HttpStatusCode.OK;
+                    response.Message = "Successfully fetched!";
+                }
+                catch (Exception ex)
+                {
+                    dbTran.Rollback();
+                    response.Message = $"{response.StatusCode} - {ex.Message}";
+                }
+            }
+
+            return response;
+        }
+
+        public VMResponse<VMReminder> Create(VMReminderReq req)
+        {
+            VMResponse<VMReminder> response = new();
+
+            using (IDbContextTransaction dbTran = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    Reminder reminder = new()
+                    {
+                        CreatedBy = req.ModifiedBy,
+                        ReminderDate = req.ReminderDate,
+                        Title = req.Title,
+                        UserId = req.UserId,
+                        IsCompleted = (req.ReminderDate.DayOfYear - DateTime.Now.DayOfYear < 0) ? true : false,
+                    };
+
+                    db.Add(reminder);
+                    db.SaveChanges();
+
+                    dbTran.Commit();
+
+                    // response
+                    response.Data = new VMReminder(reminder);
+                    response.StatusCode = HttpStatusCode.Created;
+                    response.Message = $"Reminder is successfully created!";
+                }
+                catch (Exception ex)
+                {
+                    dbTran.Rollback();
+                    response.Message = $"{response.StatusCode} - {ex.Message}";
+                }
+
+                return response;
+            }
+        }
+    }
+}
---
+++ PlannerTracker.ViewModel/VMReminder.cs
@@ -0,0 +1,38 @@
+﻿using PlannerTracker.DataModel;
+
+namespace PlannerTracker.ViewModel
+{
+    public class VMReminder
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; } = null!;
+        public DateTime ReminderDate { get; set; }
+        public bool? IsCompleted { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public Guid? ModifiedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public Guid? DeletedBy { get; set; }
+        public DateTime? DeletedOn { get; set; }
+        public bool? IsDelete { get; set; }
+
+        public VMReminder() { }
+
+        public VMReminder(Reminder db)
+        {
+            Id = db.Id;
+            UserId = db.UserId;
+            Title = db.Title;
+            ReminderDate = db.ReminderDate;
+            IsCompleted = db.IsCompleted;
+            CreatedBy = db.CreatedBy;
+            CreatedOn = db.CreatedOn;
+            ModifiedBy = db.ModifiedBy;
+            ModifiedOn = db.ModifiedOn;
+            DeletedBy = db.DeletedBy;
+            DeletedOn = db.DeletedOn;
+            IsDelete = db.IsDelete;
+        }
+    }
+}
