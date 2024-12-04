514c33e8538252542ec2ca4f329b676076e91799
femiliapm <femiliaputrimayranti@gmail.com>
2024-12-04 14:06:18 +0700

    layouting authentication for login and register

---
+++ SmartSpender.web/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartSpender.web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
