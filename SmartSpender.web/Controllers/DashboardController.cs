1ac8db316e97e02c280595da4efc86db17c43df0
femiliapm <femiliaputrimayranti@gmail.com>
2024-12-05 16:00:55 +0700

    slicing layout dashboard

---
+++ SmartSpender.web/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SmartSpender.ViewModel;
+
+namespace SmartSpender.web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            string? authStr = HttpContext.Session.GetString("auth");
+            VMAuth? auth = authStr != null ? JsonConvert.DeserializeObject<VMAuth?>(authStr) : null;
+            if (auth == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
