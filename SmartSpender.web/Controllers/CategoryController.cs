2b55abfdea17b84b07fbe52872c70fcdf6ffe9a7
femiliapm <femiliaputrimayranti@gmail.com>
2024-12-09 11:22:52 +0700

    integrate create category

---
+++ SmartSpender.web/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SmartSpender.ViewModel;
+using SmartSpender.web.Models;
+
+namespace SmartSpender.web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CategoryModel category;
+
+        public CategoryController(IConfiguration _config)
+        {
+            category = new(_config);
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Manage Category";
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Title = "Add Category";
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<VMResponse<VMCategory>?> AddCategory(VMCategoryReq req)
+        {
+            string? authStr = HttpContext.Session.GetString("auth");
+            VMAuth? auth = authStr != null ? JsonConvert.DeserializeObject<VMAuth?>(authStr) : null;
+            if (auth == null)
+            {
+                throw new Exception("Error not permission!");
+            }
+            req.ModifiedBy = auth.Id;
+
+            VMResponse<VMCategory>? response = await category.Create(req, auth.Token!);
+
+            return response;
+        }
+    }
+}
